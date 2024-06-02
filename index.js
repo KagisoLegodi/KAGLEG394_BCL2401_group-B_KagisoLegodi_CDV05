13a5f26cace645863f4cb0f18c4c5dd06481218c
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-06-02 18:56:44 +0200

    Made the navigation menu open and close when I click the button. Also, I stopped the page from scrolling when the menu is open

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+  document.body.classList.toggle('nav-open');
+
+  // Disable scrolling when the menu is open
+  document.body.style.overflow = document.body.classList.contains('nav-open') ? 'hidden' : 'auto';
 });
 
 navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+
+    // Re-enable scrolling after closing the menu
+    document.body.style.overflow = 'auto';
+  });
+});
